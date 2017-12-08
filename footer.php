fcf0b4bae7c2c2476eb97fbea6e918459b7b1384
Gourav Tyagi <gourav@inkhive.com>
2017-12-08 10:48:58 +0530

    bug fixed.

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@
 <?php get_sidebar('footer'); ?>
 <footer id="colophon" class="site-footer" role="contentinfo">
     <div class="site-info container">
-        <?php printf( __( 'Powered by %1$s.', 'ranker' ), '<a href="'.esc_url("https://inkhive.com/product/ranker/").'" target="blank" rel="nofollow">ranker Theme</a>' ); ?>
+        <?php printf( __( 'Powered by %1$s.', 'ranker' ), '<a href="'.esc_url("https://inkhive.com/product/ranker/").'" target="blank" rel="nofollow">Ranker Theme</a>' ); ?>
         <span class="sep"></span>
         <?php echo ( esc_html(get_theme_mod('ranker_footer_text')) == '' ) ? ('&copy; '.date('Y').' '.get_bloginfo('name').__('. All Rights Reserved. ','ranker')) : esc_html( get_theme_mod('ranker_footer_text') ); ?>
     </div><!-- .site-info -->
